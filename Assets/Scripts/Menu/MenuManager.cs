388876b5135b81cac36a8b202f36420badbe7f7a
Gableb <gableb@hotmail.fr>
2020-02-16 00:02:12 -0500

    added menu draft

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class MenuManager : MonoBehaviour
 {
     public Panel currentPanel = null;
     public List<Panel> panelHistory = new List<Panel>();
-
     private void Start(){
         SetupPanels();
     }
@@ -17,6 +17,35 @@ public class MenuManager : MonoBehaviour
             panel.Setup(this);
         }
 
+        currentPanel.Show();
+    }
+
+    private void Update(){
+        if(SteamVR_Actions._default.GrabGrip.state){
+            GoToPrevious();
+        }
+
+    }
+
+    public void GoToPrevious(){
+        if(panelHistory.Count == 0){
+            Debug.Log("Back pressed when history is empty");
+            Debug.Log("We should probably ask to quit app");
+            return;
+        }
+        int lastIndex = panelHistory.Count - 1;
+        SetCurrent(panelHistory[lastIndex]);
+        panelHistory.RemoveAt(lastIndex);
+    }
+
+    public void SetCurrentWithHistory(Panel newPanel){
+        panelHistory.Add(currentPanel);
+        SetCurrent(newPanel);
+    }
+
+    private void SetCurrent(Panel newPanel){
+        currentPanel.Hide();
+        currentPanel = newPanel;
         currentPanel.Show();
     }
 }
