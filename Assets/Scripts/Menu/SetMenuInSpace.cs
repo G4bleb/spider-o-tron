446b5a598542c87ebdc37191d7b7025ae7dc8e71
Gableb <gableb@hotmail.fr>
2020-02-18 18:14:30 -0500

    menu is hidden on start

--- Assets/Scripts/Menu/SetMenuInSpace.cs
+++ Assets/Scripts/Menu/SetMenuInSpace.cs
@@ -8,13 +8,13 @@ public class SetMenuInSpace : MonoBehaviour
     public SteamVR_LaserPointer steamVrLaserPointer;
     public SteamVR_Action_Boolean menuBtnAction;
     private Canvas canvas = null;
-    public float distance = 1f;
     // Start is called before the first frame update
-    void OnEnable()
+
+    private void Start()
     {
         canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
         menuBtnAction.AddOnStateDownListener(OnMenuBtnActionDown, SteamVR_Input_Sources.Any);
-
     }
 
     private void OnMenuBtnActionDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
